7222207d819aaddd4c716f4dc3b2326cbd9821e2
kurisu <callmekurisu@gmail.com>
2018-11-15 15:59:54 -0600

    "updated environment for axios"

---
+++ client/src/Axios/Environment.js
@@ -0,0 +1,9 @@
+const dev = {
+    context: "http://localhost:8080/"
+   };
+   
+   const prod = {
+    context: "1810-nchristian-projectone.rwegmths2p.us-west-2.elasticbeanstalk.com/"
+   };
+   
+   export const environment = process.env.NODE_ENV === "production" ? prod : dev;
